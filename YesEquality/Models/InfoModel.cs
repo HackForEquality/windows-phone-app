67ccbaae4d0e8a13938d61857f8105a712fae4a7
Neil Turner <neilturner@gmail.com>
2015-04-08 18:54:14 +0100

    Added initial text and assets to Info View

---
+++ YesEquality/Models/InfoModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YesEquality.Models
+{
+    public class InfoModel
+    {
+        public string Title { get; set; }
+        public Uri ImagePath { get; set; }
+        public string ImageText { get; set; }
+        public string BackgroundColour { get; set; }
+    }
+}
--- YesEquality/ViewModels/InfoViewModel.cs
+++ YesEquality/ViewModels/InfoViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using Caliburn.Micro;
+using PropertyChanged;
 using System;
+using System.Collections.ObjectModel;
+using YesEquality.Models;
 
 namespace YesEquality.ViewModels
 {
+    [ImplementPropertyChanged]
     public class InfoViewModel : Screen
     {
+        public ObservableCollection<InfoModel> InfoList { get; set; }
+        
         public InfoViewModel()
         {
+            InfoList = new ObservableCollection<InfoModel>();
 
+            InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/1_Over18.png", UriKind.Relative), ImageText = "Over 18", BackgroundColour = "#7f4097" });
+            InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/2_IrishCitizen.png", UriKind.Relative), ImageText = "Irish Citizen", BackgroundColour = "#1b75bb" });
         }
     }
 }
