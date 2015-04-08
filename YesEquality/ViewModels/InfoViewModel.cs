9d8ee966e6b03fa7f04f543014ab2b8f0a2f122c
Neil Turner <neilturner@gmail.com>
2015-04-08 23:36:23 +0100

    Add the rest of the content to Info View

--- YesEquality/ViewModels/InfoViewModel.cs
+++ YesEquality/ViewModels/InfoViewModel.cs
@@ -2,6 +2,8 @@
 using PropertyChanged;
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Controls;
 using YesEquality.Models;
 
 namespace YesEquality.ViewModels
@@ -17,6 +19,15 @@ namespace YesEquality.ViewModels
 
             InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/1_Over18.png", UriKind.Relative), ImageText = "Over 18", BackgroundColour = "#7f4097" });
             InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/2_IrishCitizen.png", UriKind.Relative), ImageText = "Irish Citizen", BackgroundColour = "#1b75bb" });
+            InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/3_Resident.png", UriKind.Relative), ImageText = "Resident in the republic", BackgroundColour = "#2e358e" });
+            InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/4_Registered.png", UriKind.Relative), ImageText = "Registered to vote", BackgroundColour = "#9e2064" });
+            InfoList.Add(new InfoModel() { Title = "Check to see if you are registered", ImagePath = new Uri("/Resources/Assets/Info/5_Check.png", UriKind.Relative), ImageText = "checktheregister.ie", BackgroundColour = "#292561" });
+            InfoList.Add(new InfoModel() { Title = "Registering is easy", ImagePath = new Uri("/Resources/Assets/Info/6_Download.png", UriKind.Relative), ImageText = "Download the form", BackgroundColour = "#009348" });
+            InfoList.Add(new InfoModel() { Title = "Registering is easy", ImagePath = new Uri("/Resources/Assets/Info/7_Fillin.png", UriKind.Relative), ImageText = "Get it signed & stamped by a Garda", BackgroundColour = "#91288c" });
+            InfoList.Add(new InfoModel() { Title = "Registering is easy", ImagePath = new Uri("/Resources/Assets/Info/8_PostForms.png", UriKind.Relative), ImageText = "Return it to your local authority post office", BackgroundColour = "#be202e" });
+            InfoList.Add(new InfoModel() { Title = "Student?", ImagePath = new Uri("/Resources/Assets/Info/9_Student.png", UriKind.Relative), ImageText = "You can vote by post", BackgroundColour = "#7bc043" });
+            InfoList.Add(new InfoModel() { Title = "Away at work?", ImagePath = new Uri("/Resources/Assets/Info/10_AtWork.png", UriKind.Relative), ImageText = "You can vote by post too!", BackgroundColour = "#7f4097" });
+            InfoList.Add(new InfoModel() { Title = "Changed address?", ImagePath = new Uri("/Resources/Assets/Info/11_Address.png", UriKind.Relative), ImageText = "You can vote by post", BackgroundColour = "#1b75bb" });
         }
     }
 }
--- YesEquality/Views/InfoView.xaml.cs
+++ YesEquality/Views/InfoView.xaml.cs
@@ -6,7 +6,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 
 namespace YesEquality.Views
 {
