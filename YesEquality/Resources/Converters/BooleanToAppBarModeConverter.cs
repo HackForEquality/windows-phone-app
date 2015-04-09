c26b4034b0f0e9c4c274973fa0da12fcf0eb7ea2
Neil Turner <neilturner@gmail.com>
2015-04-09 16:52:26 +0100

    Link button added to Info view

---
+++ YesEquality/Resources/Converters/BooleanToAppBarModeConverter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Phone.Shell;
+using System;
+using System.Windows.Data;
+
+namespace YesEquality.Resources.Converters
+{
+    public class BooleanToAppBarModeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ((bool)value) ? ApplicationBarMode.Default : ApplicationBarMode.Minimized;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- YesEquality/ViewModels/InfoViewModel.cs
+++ YesEquality/ViewModels/InfoViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using Microsoft.Phone.Tasks;
 using PropertyChanged;
 using System;
 using System.Collections.ObjectModel;
@@ -9,14 +10,20 @@ using YesEquality.Models;
 namespace YesEquality.ViewModels
 {
     [ImplementPropertyChanged]
-    public class InfoViewModel : Screen
+    public class InfoViewModel : Screen, IHandle<WebBrowserTask>
     {
+        private readonly IEventAggregator eventAggregator;
+        private readonly INavigationService navigationService;
         public ObservableCollection<InfoModel> InfoList { get; set; }
-        
-        public InfoViewModel()
-        {
-            InfoList = new ObservableCollection<InfoModel>();
+        public bool ExpandAppBar { get; set; }
 
+        public InfoViewModel(INavigationService navigationService, IEventAggregator eventAggregator)
+        {
+            this.navigationService = navigationService;
+            this.eventAggregator = eventAggregator;
+            eventAggregator.Subscribe(this);
+
+            InfoList = new ObservableCollection<InfoModel>();
             InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/1_Over18.png", UriKind.Relative), ImageText = "Over 18", BackgroundColour = "#7f4097" });
             InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/2_IrishCitizen.png", UriKind.Relative), ImageText = "Irish Citizen", BackgroundColour = "#1b75bb" });
             InfoList.Add(new InfoModel() { Title = "To vote on May 22nd you must be:", ImagePath = new Uri("/Resources/Assets/Info/3_Resident.png", UriKind.Relative), ImageText = "Resident in the republic", BackgroundColour = "#2e358e" });
@@ -28,6 +35,39 @@ namespace YesEquality.ViewModels
             InfoList.Add(new InfoModel() { Title = "Student?", ImagePath = new Uri("/Resources/Assets/Info/9_Student.png", UriKind.Relative), ImageText = "You can vote by post", BackgroundColour = "#7bc043" });
             InfoList.Add(new InfoModel() { Title = "Away at work?", ImagePath = new Uri("/Resources/Assets/Info/10_AtWork.png", UriKind.Relative), ImageText = "You can vote by post too!", BackgroundColour = "#7f4097" });
             InfoList.Add(new InfoModel() { Title = "Changed address?", ImagePath = new Uri("/Resources/Assets/Info/11_Address.png", UriKind.Relative), ImageText = "You can vote by post", BackgroundColour = "#1b75bb" });
+
+            ExpandAppBar = false;
         }
+
+        #region Commands
+        public void SelectionChanged(SelectionChangedEventArgs e)
+        {
+            var item = e.AddedItems[0] as InfoModel;
+
+            // Only show browser button when on a certain slide
+            if (item.ImageText.Contains("checktheregister"))
+            {
+                ExpandAppBar = true;
+            }
+            else
+            {
+                ExpandAppBar = false;
+            }
+        }
+
+        public void GoToWebsite()
+        {
+            eventAggregator.RequestTask<WebBrowserTask>(x => { x.Uri = new Uri("https://www.checktheregister.ie/"); });
+        }
+
+        public void Handle(WebBrowserTask message)
+        {
+        }
+
+        public void GoToAbout()
+        {
+            navigationService.UriFor<AboutViewModel>().Navigate();
+        }
+        #endregion
     }
 }
